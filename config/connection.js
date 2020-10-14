b22b8f8ae4228f894166c4f223b98f8364845fe1
Andrew Crow <andrew@crow.me>
2020-10-14 15:08:13 -0700

    added mysql connection to connedtion.js

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// Dependencies
+// ===========================================================
+const util = require("util");
+const mysql = require("mysql");
+
+// Creating a connection to MySql Server
+// ===========================================================
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "eat_da_burger_db"
+});
+
+connection.connect();
+connection.query = util.promisify(connection.query);
+
+// Exporting this connection to be used in sql queries 
+// ===========================================================
+module.exports = connection;
