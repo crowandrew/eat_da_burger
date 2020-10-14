5232203b6775271cbdb694f668d33cce635f6273
Andrew Crow <andrew@crow.me>
2020-10-14 15:05:00 -0700

    added schema.sql seeds.sql and connection.js

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+### Schema
+
+CREATE DATABASE eat_da_burger_db;
+USE eat_da_burger_db;;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (name,devoured) VALUES ('Krusty Burger',false);
+INSERT INTO burgers (name,devoured) VALUES ('Ghost Burger',true);
+INSERT INTO burgers (name,devoured) VALUES ('Varsity Burger',false);
+INSERT INTO burgers (name,devoured) VALUES ('Umami Burger',true);
+INSERT INTO burgers (name,devoured) VALUES ("Ray's Hell Burger",false);
+INSERT INTO burgers (name,devoured) VALUES ('Ramen Burger',true);
+INSERT INTO burgers (name,devoured) VALUES ('MOS Burger',false);
+INSERT INTO burgers (name,devoured) VALUES ('Jucy Lucy Burger',true);
+
+
