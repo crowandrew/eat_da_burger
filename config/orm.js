4db53d8d3e0a860259a92398db78f551e659e603
Andrew Crow <andrew@crow.me>
2020-10-14 15:55:15 -0700

    added orm object and refactored all and create functions

--- config/orm.js
+++ config/orm.js
@@ -24,3 +24,55 @@ const objToSql = object => {
     return arr.toString();
 }
 
+// orm object for all our SQL statement functions.
+// ===========================================================
+const orm = {
+    all: function (tableInput, callback) {
+        connection.query(`SELECT * FROM ${tableInput};`, function (err, result) {
+            if (err) throw err;
+            callback(result);
+        });
+    },
+    create: function (table, cols, vals, callback) {
+        connection.query(`INSERT INTO ${table} (${cols.toString()}) VALUES (${printQuestionMarks(vals.length)});`, vals, function (err, result) {
+            if (err) throw err;
+            callback(result);
+        });
+    },
+    // An example of objColVals would be {name: panther, sleepy: true}
+    update: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    delete: function (table, condition, cb) {
+        var queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+};
+
+
+// Export the orm object for the model (burgers_model.js).
+module.exports = orm;
