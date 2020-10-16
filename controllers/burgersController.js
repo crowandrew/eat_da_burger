71c02f571a8c536ef2c9703f80e2ba236c40ea36
Andrew Crow <andrew@crow.me>
2020-10-15 21:38:22 -0700

    add route for / root

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -5,7 +5,19 @@ const router = express.Router();
 
 // Importing the model burger.js to use the database functions
 // ===========================================================
-const cat = require("../models/burger");
+const burger = require("../models/burger");
+
+// Create all our routes and set up logic 
+// ===========================================================
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      const handleBarsObject = {
+        burgers: data
+      };
+      console.log(handleBarsObject);
+      res.render("index", handleBarsObject);
+    });
+  });
 
 // Exporting the routes for server.js
 module.exports = router;
