1e43f4c8787f18058c8cea52f0b13dbf1bce0f8d
Andrew Crow <andrew@crow.me>
2020-10-15 21:49:35 -0700

    setup imports, express, handlebars and listner

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 // Dependencies
 // ===========================================================
+const express = require("express");
 
 // Sets up the Express App
 // ===========================================================
+const PORT = process.env.PORT || 8080;
+const app = express();
+app.use(express.static("public"));
 
 // Sets up the Express app to handle data parsing
 // ===========================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Sets up Handlebars
+// ===========================================================
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Routes
 // ===========================================================
 
+
 // Listener
 // ===========================================================
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
