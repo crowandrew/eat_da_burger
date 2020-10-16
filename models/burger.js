3955b6ac114236e789be317112b6ec9399cefec6
Andrew Crow <andrew@crow.me>
2020-10-15 21:30:33 -0700

    finished cuntions for burger and CRUD

--- models/burger.js
+++ models/burger.js
@@ -2,26 +2,30 @@
 // ===========================================================
 const orm = require("../config/orm");
 
+// object to hold the CRUD functions
+// ===========================================================
 const burger = {
-    all: function(callback) {
-        orm.all("burgers", function(res) {
-          callback(res);
+    all: function (callback) {
+        orm.all("burgers", function (res) {
+            callback(res);
         });
-      },
-      create: function(cols, values, callback) {
-        orm.create("burgers", cols, values, function(res) {
-          callback(res);
+    },
+    create: function (cols, values, callback) {
+        orm.create("burgers", cols, values, function (res) {
+            callback(res);
         });
-      },
-      update: function(objColValues, condition, callback) {
-        orm.update("cats", objColValues, condition, function(res) {
-          callback(res);
+    },
+    update: function (objColValues, condition, callback) {
+        orm.update("burgers", objColValues, condition, function (res) {
+            callback(res);
         });
-      },
-      
+    },
+    delete: function (condition, callback) {
+        orm.delete("burgers", condition, function (res) {
+            callback(res);
+        });
+    }
 };
-
-
 
 // Export the burger variable with functions for the burger controller 
 // ===========================================================
