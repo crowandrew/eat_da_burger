edcd367cbf8de22b5f074a27e4dba348825455b2
Andrew Crow <andrew@crow.me>
2020-10-15 21:23:19 -0700

    added inmport and export to burger.js

---
+++ models/burger.js
@@ -0,0 +1,12 @@
+// Importing the ORM to interact with the database.
+// ===========================================================
+const orm = require("../config/orm");
+
+
+
+
+
+// Export the burger variable with functions for the burger controller 
+// ===========================================================
+module.exports = burger;
+
