86d0a7a7c3e3d6302a3eb5967ec5ce5b77ed543a
Andrew Crow <andrew@crow.me>
2020-10-15 21:34:39 -0700

    created imports and exports for burgers Controller

---
+++ controllers/burgersController.js
@@ -0,0 +1,11 @@
+// Dependencies
+// ===========================================================
+const express = require("express");
+const router = express.Router();
+
+// Importing the model burger.js to use the database functions
+// ===========================================================
+const cat = require("../models/burger");
+
+// Exporting the routes for server.js
+module.exports = router;
