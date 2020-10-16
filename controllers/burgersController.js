0f56d364c3ae05fd94a3db0193ce1c651365127f
Andrew Crow <andrew@crow.me>
2020-10-15 21:40:38 -0700

    added post route

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -19,5 +19,19 @@ router.get("/", function(req, res) {
     });
   });
 
+  router.post("/api/burgers", function(req, res) {
+    burger.create([
+      "name", "devoured"
+    ], [
+      req.body.name, req.body.devoured
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+
+  
+
+
 // Exporting the routes for server.js
 module.exports = router;
