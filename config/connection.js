67a9f590adca533de2b27631c486aa6e496c5b27
Andrew Crow <andrew@crow.me>
2020-10-16 00:24:46 -0700

    added heroku jawsdb info

--- config/connection.js
+++ config/connection.js
@@ -5,13 +5,20 @@ const mysql = require("mysql");
 
 // Creating a connection to MySql Server
 // ===========================================================
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "eat_da_burger_db"
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "eat_da_burger_db"
+    });
+};
+
 
 connection.connect();
 connection.query = util.promisify(connection.query);
