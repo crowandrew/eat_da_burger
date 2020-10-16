7f074cda7552bb29826e568c44b2ae7ffdf4299a
Andrew Crow <andrew@crow.me>
2020-10-15 21:42:38 -0700

    added post route

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -9,28 +9,41 @@ const burger = require("../models/burger");
 
 // Create all our routes and set up logic 
 // ===========================================================
-router.get("/", function(req, res) {
-    burger.all(function(data) {
-      const handleBarsObject = {
-        burgers: data
-      };
-      console.log(handleBarsObject);
-      res.render("index", handleBarsObject);
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        const handleBarsObject = {
+            burgers: data
+        };
+        console.log(handleBarsObject);
+        res.render("index", handleBarsObject);
     });
-  });
+});
 
-  router.post("/api/burgers", function(req, res) {
+router.post("/api/burgers", function (req, res) {
     burger.create([
-      "name", "devoured"
+        "name", "devoured"
     ], [
-      req.body.name, req.body.devoured
-    ], function(result) {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
+        req.body.name, req.body.devoured
+    ], function (result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
     });
-  });
+});
 
-  
+router.put("/api/burgers/:id", function (req, res) {
+    const condition = "id = " + req.params.id;
+    console.log("condition", condition);
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
 
 
 // Exporting the routes for server.js
